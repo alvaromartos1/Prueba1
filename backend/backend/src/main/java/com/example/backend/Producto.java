f0c311040eb485f45425c9438fccd692d748b80e
alvaromartos1 <alvaromartos2011@hotmail.com>
2023-06-21 22:42:37 +0200

    clase producto

---
+++ backend/backend/src/main/java/com/example/backend/Producto.java
@@ -0,0 +1,67 @@
+package com.example.backend;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY, getterVisibility = JsonAutoDetect.Visibility.NONE, setterVisibility = JsonAutoDetect.Visibility.NONE)
+
+public class Producto {
+
+    @JsonProperty("nombre")
+    public String  nombre;
+    @JsonProperty("categoria")
+    public String categoria;
+    @JsonProperty("precio")
+    public double precio;
+    @JsonProperty("codigo")
+    public String codigo;
+
+    public Producto(String nombre, String categoria, double precio, String codigo) {
+        this.nombre = nombre;
+        this.categoria = categoria;
+        this.precio = precio;
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    @Override
+    public String toString() {
+        return "Producto{" +
+                "nombre='" + nombre + '\'' +
+                ", categoria='" + categoria + '\'' +
+                ", precio=" + precio +
+                ", codigo='" + codigo + '\'' +
+                '}';
+    }
+}
