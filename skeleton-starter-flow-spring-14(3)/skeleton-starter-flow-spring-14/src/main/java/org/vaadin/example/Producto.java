3c80edc6097edb130f72149e6c4135a94baea8dc
alvaromartos1 <alvaromartos2011@hotmail.com>
2023-06-23 20:34:32 +0200

    clase producto

---
+++ skeleton-starter-flow-spring-14(3)/skeleton-starter-flow-spring-14/src/main/java/org/vaadin/example/Producto.java
@@ -0,0 +1,70 @@
+package org.vaadin.example;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY, getterVisibility = JsonAutoDetect.Visibility.NONE, setterVisibility = JsonAutoDetect.Visibility.NONE)
+
+public class Producto {
+
+
+    @JsonProperty("nombre")
+    public String  nombre;
+    @JsonProperty("categoria")
+    public String categoria;
+    @JsonProperty("precio")
+    public double precio;
+    @JsonProperty("EAN13")
+    public String EAN13;
+
+
+    public Producto(String nombre, String categoria, double precio, String EAN13) {
+        this.nombre = nombre;
+        this.categoria = categoria;
+        this.precio = precio;
+        this.EAN13 = EAN13;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public String getEAN13() {
+        return EAN13;
+    }
+
+    public void setEAN13(String EAN13) {
+        this.EAN13 = EAN13;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Producto{" +
+                "nombre='" + nombre + '\'' +
+                ", categoria='" + categoria + '\'' +
+                ", precio=" + precio +
+                ", EAN13='" + EAN13 + '\'' +
+                '}';
+    }
+}
